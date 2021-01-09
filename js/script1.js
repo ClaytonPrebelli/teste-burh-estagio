0435d859511e5ceef1c43bb518d7b332bd006381
Clayton Prebelli <clayton.pires@aluno.unip.br>
2021-01-09 12:13:24 -0300

    ajustado tela principal
    Tela principal terminada de ajustar animações e elementos. proximo passo, resumo total do filme, para a seguir criar esquema de usuarios e favoritos

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,6 @@
+let clicado=0;
 function pesquisar(){
+    
     var url = "http://www.omdbapi.com/";
     var titulo = document.getElementById("pesquisa").value;
     var chave = "49f3de13";
@@ -7,24 +9,47 @@ function pesquisar(){
     var i = 0;
     var title = document.getElementById(classe[i]).innerHTML="";
     document.getElementById("year").innerHTML="";
-    
-  
-
+    let capa = document.getElementById("capa");
+        function ver_capa(data){
+        if(data!="N/A"){
+    $("#capa").attr('src',data);}else{
+        capa.src = '/images/no_poster.png';
+    }
+}
             $.ajax({
-           url : requisito,
+            url : requisito,
             type : "get",
             datatype : "json",
-            success : function(data){
-                console.log(data);
+                success : function(data){            
             $("#title").append(data.Title);    
             $("#year").append(data.Year);
-            $("#capa").attr('src',data.Poster);
-                   
+            ver_capa(data.Poster);
+            alertaretorno(data.Response);
             },
                 error : function(erro){
-                    console.log(erro);
+                    alert(erro);
                 }
             
-       })
+       });
+       function alertaretorno(data){
+           
+           if (data=="False"){
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'O filme não foi encontrado, tente novamente!',})
+           }else{
+               if(clicado!=1){
+               let aparece_ficha = document.getElementById("ficha");
+               aparece_ficha.style.animation = "";
+               setTimeout(() => aparece_ficha.style.animation = "aparecer_ficha 3s ease 1s forwards",2);
+               let pesq = document.getElementById("sec_principal");
+               pesq.style.animation = "";
+               setTimeout(() => pesq.style.animation = "deslizar_cima forwards 3s", 2);
+               }
+           }
+        clicado=1}}
        
-    };
+    
+       
+    
